e4924fbc3a1b434fdc3eb426aaab37f17f0da38b
Gayane Avetyan <gayane_avetyan@epam.com>
2024-11-28 17:33:13 +0400

    sss

--- task05/dnapp/lambdas/api_handler/obj/Debug/net8.0/Function.AssemblyInfo.cs
+++ task05/dnapp/lambdas/api_handler/obj/Debug/net8.0/Function.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleLambdaFunction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b1116edd68ea9e6c3f8e8b246d565b58c78a13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cd090e4b418b1ed26513b2642093eea51401170")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleLambdaFunction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleLambdaFunction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
